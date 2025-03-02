7d8689b4c3eadb3422ee5eaf4834c09300375b9e
As1lbekOd1ljonov <aslbekoo176@gmail.com>
2025-03-02 12:25:03 +0500

    vazifa

--- config/config/settings.py
+++ config/config/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 
 
-from django.conf.global_settings import MEDIA_URL, STATICFILES_DIRS
+from django.conf.global_settings import MEDIA_URL, STATICFILES_DIRS, EMAIL_BACKEND, EMAIL_HOST, EMAIL_USE_TLS, \
+    EMAIL_PORT, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,3 +136,12 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'rustamovasilbek1221@gmail.com'
+EMAIL_HOST_PASSWORD = 'vzzi niqa nvxl boxj'
